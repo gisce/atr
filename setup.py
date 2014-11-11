4db5c70e89bcf93a945f895ee0b1735f3b935077
Eduard Carreras <ecarreras@gisce.net>
2014-11-11 12:47:05 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,15 @@ INSTALL_REQUIRES = [
 
 setup(
     name='atr',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     url='http://git.gisce.lan/atr',
     license='MIT',
+    install_requires=INSTALL_REQUIRES,
+     entry_points="""
+        [console_scripts]
+        atr=atr.cli:atr
+    """,
     author='GISCE-TI, S.L.',
     author_email='devel@gisce.net',
     description='ATR Utils'
